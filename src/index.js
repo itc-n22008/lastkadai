d1ff48ef2e9a338208a6d7e1edc5ee3b56e91c8e
itc-n22008 <n22008@std.it-college.ac.jp>
2023-02-23 09:28:02 +0000

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Number from './number.js'
+
+<Number />
+
+// ========================================
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Game />);
